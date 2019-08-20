a15b0cac82cc78d07c12bdfcfd598719fcf91f9f
renatopcinfo <renatopcinfo@github.com>
2019-08-20 01:31:29 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    env: {
+      development: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['./src']
+            }
+          ]
+        ]
+      }
+    }
+  };
+};
